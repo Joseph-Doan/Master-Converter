a0809a51556b5f90e94a0563a5632c4b3e9f7525
Joseph Doan <Joseph.H.Doan@gmail.com>
2015-01-02 21:36:39 -0800

    Initial Commit

---
+++ Master Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Master Converter
+//
+//  Created by Joseph Doan on 1/2/15.
+//  Copyright (c) 2015 Joseph Doan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
