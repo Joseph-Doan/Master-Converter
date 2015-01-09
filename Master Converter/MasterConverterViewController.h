5455d697cc1286eb54fdb800bb7bfd742128270d
Joseph Doan <Joseph.H.Doan@gmail.com>
2015-01-08 21:18:32 -0800

    Replaced default ViewController with MasterConverterViewController and added UI elements to storyboard

---
+++ Master Converter/MasterConverterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterConverterViewController.h
+//  Master Converter
+//
+//  Created by Joseph Doan on 1/8/15.
+//  Copyright (c) 2015 Joseph Doan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterConverterViewController : UIViewController
+
+@end
